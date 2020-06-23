97b05ae5bd309842816e665142a917708fd2217c
mysongzihao <www.2367474033@qq.com>
2020-06-23 09:16:55 +0800

    620

---
+++ songzihao/6.20/src/APP.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<my-cert></my-cert>
+   
+		<my-slider></my-slider>
+	</div>
+</template>
+
+<script>
+	import myCert from './Transfet.vue'
+	import mySlider from './Slider.vue'
+	export default {
+		name: 'App',
+		components: {
+			myCert,
+			mySlider
+		},
+		
+	}
+</script>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		text-decoration: none;
+	}
+	#app{
+		margin-top: 20px;
+		width: 1000px;
+		height: 1000px;
+	}
+</style>
---
+++ songzihao/6.20/src/Transfet.vue
@@ -0,0 +1,88 @@
+<template>
+	<div>
+		<ul>
+			<span>全选</span>
+			<li v-for="(item,$index) in left" :key='item' @click="lnum=$index" :class="lnum==$index?'active':''">{{item}}</li>
+		</ul>
+		<input type="button" value="<" @click='rh' />   
+		<input type="button" value=">" @click='le'/>
+		<ul>
+			<span>全选</span>
+			<li v-for="(i,$index) in right" :key="i" @click="rnum=$index" :class="rnum==$index?'active':''">{{i}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+				return {
+					lnum: 0,
+					rnum: 0,
+					left: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+					right: [3, 6, 8, 4, 2]
+				}
+			},
+			methods: {
+				
+				le() {
+					
+					this.left.forEach(item => {
+						this.right.push(this.left[item]);
+						this.left.splice(item,1);
+					});
+				},
+				rh(){
+					this.right.forEach(item => {
+						this.left.push(this.left[item]);
+						this.right.splice(item,1);
+					});
+				}
+			}
+	}
+</script>
+
+<style scoped="scoped">
+	* {
+		list-style: none;
+		padding: 0;
+	}
+	div{
+		margin-right: 50px;
+	}
+	ul {
+		float: left;
+		width: 150px;
+		border: 1px solid gainsboro;
+		border-radius: 5px;
+	}
+	
+	li {
+		width: 150px;
+		text-align: center;
+		margin-top: 2px;
+	}
+	
+	input {
+		float: left;
+		margin-top: 60px;
+		width: 40px;
+		height: 40px;
+		background-color: dodgerblue;
+		color: white;
+		font-size: 20px;
+		border: none;
+		border-radius: 5px;
+		margin: 50px 5px 0 5px;
+	}
+	
+	span {
+		margin-left: 40%;
+		text-align: center;
+	}
+	
+	.active {
+		color: cornflowerblue;
+		background-color: gainsboro;
+	}
+</style>
